f852bc8c61f091fb16f064f906fc6e913ad14a88
Hannah Elarabawy <helarabawy@ucla.edu>
2017-02-20 21:15:41 -0800

    Extra info

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -89,3 +89,51 @@ bool StudentWorld::loadField()
 	
 	// TODO: LOAD STUFF
 }
+
+
+/*
+As you can see, the data file contains a 64x64 grid of different characters that represent the different objects in the simulation.  Valid characters for your field data file are: 
+  
+‘*’ characters designate pebbles which block movement of all insects . Every field must have its perimeter consist entirely of pebbles. 
+ 
+‘g’ characters specify the starting locations of baby grasshoppers. 
+ 
+‘0’, ‘1’, ‘2’ and ‘3’ specify the location of the four ant colonies’ anthills. 
+ 
+‘w’ characters specify pools of water. 
+ 
+‘f’ characters specify piles of food. 
+ 
+‘p’ characters specify poison. 
+  
+Space characters represent empty locations. 
+*/
+
+
+
+
+
+/*
+#include "Field.h"   // you must include this file to use our Field class 
+… 
+int StudentWorld::someFunctionYouWriteToLoadTheField() 
+{ 
+   string fieldFileName; 
+   Field f; 
+ 
+   std::string fieldFile = getFieldFilename(); 
+   bool loadedFieldFileSuccessfully = f.loadField(fieldFile); 
+               if ( ! f.loadField(fieldFile) )        return false;     // something bad happened!   
+   // otherwise the load was successful and you can access the 
+   // contents of the field – here’s an example 
+     int x = 0;    int y = 5; 
+   Field::FIELDITEM item = f.getContentsOf(x,y); // note it’s x,y and not y,x!!!    if (item == Field::FIELDITEM::rock) 
+       cout << "A pebble should be placed at 0,5 in the field\n";     x = 10;    y = 7; 
+   item = f.getContentsOf(x, y);    if (item == FIELDITEM::anthill0)        cout << "The anthill for the first Ant should be at 10,7 in the field\n":     … // etc. 
+} 
+  
+Notice that the getContentsOf() method takes the column parameter (x) first, then the row parameter (y) second.  This is different than the order one normally uses when indexing a 2dimensional array, which would be array[row][col]. Be careful! 
+  
+Hint: You will presumably want to use our Field class in your StudentWorld’s init() method. 
+
+*/
